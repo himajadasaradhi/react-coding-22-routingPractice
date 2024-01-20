e6305f358511d8966309ac86023d81e1e02668c1
himaja dasaradhi <himajadasaradhi2000@gmail.com>
2024-01-20 15:28:41 +0000

    completed

---
+++ src/components/Contact/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+import './index.css'
+
+const Contact = () => (
+  <div className="contact-align">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/contact-blog-img.png"
+      alt="contact"
+      className="contact"
+    />
+    <h1>Contact</h1>
+  </div>
+)
+
+export default Contact
